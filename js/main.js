2c3603f9b122b7a1fd4197c153c73f7ac4cbbf3a
dayant-wood <dayant.wood@gmail.com>
2021-03-05 11:08:51 +0300

    create: swiper

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  
+});
