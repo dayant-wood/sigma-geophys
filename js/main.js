f30c12412eb9c08c8b8cba3c0aa8f13693230401
dayant-wood <dayant.wood@gmail.com>
2021-04-01 16:08:45 +0300

    create: create methods & contacts

--- js/main.js
+++ js/main.js
@@ -15,10 +15,10 @@ $(document).ready(function() {
             onlyInViewport: false,
         },
 
-        autoplay: {
-            delay: 5000,
-            disableOnInteraction: true,
-        },
+        // autoplay: {
+        //     delay: 5000,
+        //     disableOnInteraction: true,
+        // },
 
     });
 
@@ -47,6 +47,27 @@ $(document).ready(function() {
             mobileMenu.removeClass("mobile--visible");
             $("body").toggleClass("hidden");
         }
+    });
+
+    //TABS
+
+    $(function() {
+        var tab = $('#tabs .tabs-items > div');
+        tab.hide().filter(':first').show();
+
+        // Клики по вкладкам.
+        $('#tabs .tabs-nav a').click(function() {
+            tab.hide();
+            tab.filter(this.hash).show();
+            $('#tabs .tabs-nav a').removeClass('active');
+            $(this).addClass('active');
+            return false;
+        }).filter(':first').click();
+
+        // Клики по якорным ссылкам.
+        $('.tabs-target').click(function() {
+            $('#tabs .tabs-nav a[href=' + $(this).data('id') + ']').click();
+        });
     });
 
 
